591d53f7a3acb31640cb8d7371c2bf1c9027038c
refatkhan <refatkhan897@gmail.com>
2025-07-21 02:38:09 +0600

    remove await

--- index.js
+++ index.js
@@ -173,7 +173,3 @@ async function run() {
     // Ensures that the client will close when you finish/error
   }
 }
-run().catch(console.dir);
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
