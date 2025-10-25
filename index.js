c025b8d2c491696f9bdc03bc3f1a018ebe023a23
Ismail Khan Refat <refatkhan897@gmail.com>
2025-10-25 15:05:59 +0600

    fix te key

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const admin = require("firebase-admin");
  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+ serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
